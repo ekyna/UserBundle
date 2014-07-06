596a8c66b62bdf545438b6a6ff7e4c7400f753e8
Etienne Dauvergne <contact@ekyna.com>
2014-07-06 18:47:51 +0200

    Authent success handler

--- Authentication/AuthenticationSuccessHandler.php
+++ Authentication/AuthenticationSuccessHandler.php
@@ -18,6 +18,12 @@ class AuthenticationSuccessHandler extends DefaultAuthenticationSuccessHandler
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
+        if ($targetUrl = $request->getSession()->get('_ekyna.login_success.target_path')) {
+            $request->getSession()->remove('_ekyna.login_success.target_path');
+        
+            return $this->httpUtils->createRedirectResponse($request, $targetUrl);
+        }
+
         return parent::onAuthenticationSuccess($request, $token);
     }
 }
