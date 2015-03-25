5b4eb50103d2c8e202556baa14346d6afe784223
Etienne Dauvergne <contact@ekyna.com>
2015-03-25 16:13:12 +0100

    User search form type

---
+++ Form/Type/UserSearchType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Ekyna\Bundle\UserBundle\Form\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class UserSearchType
+ * @package Ekyna\Bundle\UserBundle\Form\Type
+ * @author Étienne Dauvergne <contact@ekyna.com>
+ */
+class UserSearchType extends AbstractType
+{
+    /**
+     * @var string
+     */
+    private $userClass;
+
+
+    /**
+     * Constructor.
+     *
+     * @param string $userClass
+     */
+    public function __construct($userClass)
+    {
+        $this->userClass = $userClass;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+            ->setDefaults(array(
+                'label' => 'ekyna_core.field.user',
+                'required' => true,
+                'entity'   => $this->userClass,
+                'search_route' => 'ekyna_user_user_admin_search',
+                'find_route'   => 'ekyna_user_user_admin_find',
+                'allow_clear'  => false,
+
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'ekyna_core_entity_search';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'ekyna_user_search';
+    }
+}
