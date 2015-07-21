c1a73fd299868da1428e654287aecf95c08ee894
Etienne Dauvergne <contact@ekyna.com>
2015-07-21 16:14:31 +0200

    Removed form options default value.

--- Form/Type/IdentityType.php
+++ Form/Type/IdentityType.php
@@ -16,7 +16,7 @@ class IdentityType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options = array())
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('gender', 'ekyna_user_gender', array(
