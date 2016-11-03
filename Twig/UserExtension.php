3490575246274ce0c707222a127e9c7517400ed9
Etienne Dauvergne <contact@ekyna.com>
2016-11-03 15:19:54 +0100

    [User] Removed deprecated getGlobals()

--- Twig/UserExtension.php
+++ Twig/UserExtension.php
@@ -27,13 +27,52 @@ class UserExtension extends \Twig_Extension
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritDoc
      */
-    public function getGlobals()
+    public function getFunctions()
     {
         return [
-            'ekyna_user_config' => $this->config,
+            new \Twig_SimpleFunction(
+                'ekyna_user_template_name',
+                [$this, 'getTemplateName']
+            ),
+            new \Twig_SimpleFunction(
+                'ekyna_user_account_var',
+                [$this, 'getAccountVar']
+            ),
         ];
+    }
+
+    /**
+     * Returns the template name.
+     *
+     * @param string $key
+     *
+     * @return string
+     */
+    public function getTemplateName($key)
+    {
+        if (!isset($this->config['templates'][$key])) {
+            throw new \InvalidArgumentException("Template name '$key' is not defined.");
+        }
+
+        return $this->config['templates'][$key];
+    }
+
+    /**
+     * Returns the account var.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function getAccountVar($key)
+    {
+        if (!isset($this->config['account'][$key])) {
+            throw new \InvalidArgumentException("Account var '$key' is not defined.");
+        }
+
+        return $this->config['account'][$key];
     }
 
     /**
